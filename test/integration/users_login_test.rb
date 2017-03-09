c38ce92ec4e9780f0106e37117b2e69d56b980a6
severnsc <severnsc@gmail.com>
2017-03-09 14:48:26 -0500

    Add login integration test

---
+++ test/integration/users_login_test.rb
@@ -0,0 +1,26 @@
+class UsersLoginTest < ActionDispatch::IntegrationTest
+
+	def setup
+		@user = users(:chris)
+	end
+
+	test "invalid login info" do
+		get login_path
+		assert_template 'sessions/new'
+		post login_path, params: { session: { email: @user.email,
+											  password: "bad"}}
+		assert_template 'sessions/new'
+		assert_not flash.empty?
+		assert_select 'div.alert'
+	end
+
+	test "valid login info" do
+		get login_path
+		assert_template 'sessions/new'
+		post login_path, params: { session: { email: @user.email,
+											  password: "password"}}
+		assert_redirected_to user_path(@user)
+		follow_redirect!
+		assert_template 'users/show'
+	end
+end
