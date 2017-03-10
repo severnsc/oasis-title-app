059ecb1f14131496fe4121de65553ab9b0fcc4e4
severnsc <severnsc@gmail.com>
2017-03-10 12:19:44 -0500

    Fill out static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,4 +8,31 @@ class StaticPagesController < ApplicationController
 
 	def request_quote
 	end
+
+  def about
+  end
+
+  def our_team
+  end
+
+  def our_role
+  end
+
+  def title_insurance
+  end
+
+  def real_estate_closings
+  end
+
+  def short_sale_services
+  end
+
+  def realtor_services
+  end
+
+  def lender_services
+  end
+
+  def homeowner_services
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
 
-  get 'agents/show'
+  get '/about', to: 'static_pages#about'
+
+  get '/our-team', to: 'static_pages#our_team'
+
+  get '/our-role', to:'static_pages#our_role'
+
+  get '/title-insurance', to:'static_pages#title_insurance'
+
+  get '/real-estate-closings', to:'static_pages#real_estate_closings'
+
+  get '/short-sale-services', to:'static_pages#short_sale_services'
+
+  get '/realtor-services', to:'static_pages#realtor_services'
+
+  get '/lender-services', to:'static_pages#lender_services'
+
+  get 'homeowner-services', to:'static_pages#homeowner_services'
 
   resources :title_orders
 
