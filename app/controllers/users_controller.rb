c3ce34704edcedf00f2c6ddd470fddba62565b03
severnsc <severnsc@gmail.com>
2017-03-21 14:06:30 -0400

    Add correct_user before_filter for edit, update, admin and admin invite)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 	before_action :logged_in_user, only: [:show]
 	before_action :is_admin?, only: [:admin, :admin_invite]
+	before_action :correct_user, only: [:edit, :update, :admin, :admin_invite]
 
 	def new
 		@user = User.new
@@ -83,6 +84,11 @@ class UsersController < ApplicationController
 
 	def is_admin?
 		redirect_to user_path unless current_user && current_user.admin?
+	end
+
+	def correct_user
+		user = User.find(params[:id])
+		redirect_to current_user unless user == current_user
 	end
 
 	def user_params
